82a844bc3c921b9921205ce8faf64b930c2316c4
Olimjon <161357020+Olimjon2001@users.noreply.github.com>
2024-10-09 02:16:58 +0500

    Update dastur.py

--- dastur.py
+++ dastur.py
@@ -2,20 +2,20 @@
 # print("hello wordl")
 # print("Sanaymiz 10gacha")
 # for n in range(10):
-#     print(n+1)
-# type error
-# son=input("Istalgan son kiriting")
-# son=int(son)
-# print(f"{son}sonni kvadrati{son**2}ga teng")
-# mevalar=['olma','uzum','anaor','nok']
-# print(mevalar[5])
-# radius=5
-# pi=3.14
-# aylana_yuzi=pi*radius**2
-# print(aylana_yuzi)
-# son=float(input("Istalgan son kiriting"))
-# ildiz=son**0.5
-# print(f"{son}sonning ildizi {ildiz}ga teng")
+    print(n+1)
+type error
+son=input("Istalgan son kiriting")
+son=int(son)
+print(f"{son}sonni kvadrati{son**2}ga teng")
+mevalar=['olma','uzum','anaor','nok']
+print(mevalar[5])
+radius=5
+pi=3.14
+aylana_yuzi=pi*radius**2
+print(aylana_yuzi)
+son=float(input("Istalgan son kiriting"))
+ildiz=son**0.5
+print(f"{son}sonning ildizi {ildiz}ga teng")
 mevalar=['olma','nok''anor','uzum']
 for meva in (mevalar):
     print(meva)
