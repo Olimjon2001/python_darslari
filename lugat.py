22dc42906f523d6956bf8f061ecd018f14d08851
Olimjon <161357020+Olimjon2001@users.noreply.github.com>
2024-10-24 09:46:16 +0500

    Create lugat.py

---
+++ lugat.py
@@ -0,0 +1,22 @@
+# car_1 ={'model':'jentra','rang':'qora','yil':'2024'}
+# print(car_1['model'],car_1['rang'],car_1['yil'])
+
+# mevalar={'olma':'10000','qovun':'20000','tarvuz':'50000'}
+# print(f"olmani narxi{mevalar['olma']}so`m")
+
+# talaba={'ism':"avazov olimjon",'yosh':25,'t_yil':2000}
+# print(f"{talaba['ism'].title()},\
+#       {talaba['t_yil']}-yilda tugilgan\
+#         {talaba['yosh']}yoshda")
+# talaba['kurs']=1
+# talaba['fakultet']='Intelektuala tizimlar'
+# talaba['yunalish']='suniy entelekt'
+# talaba['ism']='Abdulloh'
+# print(talaba)
+
+
+talaba = {}
+talaba['ism']="ehsmatov toshmat"
+talaba['yoshi']=20
+talaba['kurs']=2
+print(talaba)
